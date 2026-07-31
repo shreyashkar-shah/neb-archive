8b4c3158e2d2d233931aae58f8fce93e818e3e64
Shreyashkar Shah <shreyashkarshah@gmail.com>
2026-07-31 10:44:43 +0545

    Content Added

--- neb-data.mjs
+++ neb-data.mjs
@@ -93,7 +93,7 @@ export const BOARD_DATA = {
     streams: {
       Science: { subjects: {
         English: ['2083','2082','2081','2080'],
-        Mathematics: ['2083','2082','2081',{ value: '2080-Set1', label: '2080' }, { value: '2080-Set2', label: '2080' },{ value:'2080-GIE', label:'2080' },{ value:'2080-Model', label:'2080'},'2079'],
+        Mathematics: ['2083','2082',{ value: '2081-Set1', label: '2081' },{ value: '2081-Set2', label: '2081' },{ value:'2081-Supplementary', label:'2081' },{ value: '2080-Set1', label: '2080' }, { value: '2080-Set2', label: '2080' },{ value:'2080-GIE', label:'2080' },{ value:'2080-Model', label:'2080'},'2079'],
         Nepali: ['2083','2082','2081','2080'],
         Physics: ['2083','2082','2081','2080',{ value: '2079-Set1', label: '2079' },{ value: '2079-Set2', label: '2079' },{ value:'2079-Model', label:'2079' }, { value:'2078-Model', label:'2078' }],
         Chemistry: ['2083','2082','2081', { value:'2081-Supplementary', label:'2081' }, { value:'2080-GIE', label:'2080' }],
