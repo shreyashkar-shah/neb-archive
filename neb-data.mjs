9277eb8bd3cf7e6681d8bbf62dbee421b0589d7d
Shreyashkar Shah <shreyashkarshah@gmail.com>
2026-07-31 20:52:10 +0545

    Content Added

--- neb-data.mjs
+++ neb-data.mjs
@@ -98,7 +98,7 @@ export const BOARD_DATA = {
         Physics: ['2083','2082','2081','2080',{ value: '2079-Set1', label: '2079' },{ value: '2079-Set2', label: '2079' },{ value:'2079-Model', label:'2079' }, { value:'2078-Model', label:'2078' }],
         Chemistry: ['2083','2082','2081', { value:'2081-Supplementary', label:'2081' },'2080',{ value:'2080-GIE', label:'2080' },'2079',{ value:'2079-Model', label:'2079' }],
         Biology: ['2083','2082','2081'],
-        'Computer Science': ['2083','2082','2081','2080'],
+        'Computer Science': ['2083','2082','2081','2080',{ value:'2079-Model', label:'2079' }],
       } },
       Management: { subjects: {
         English: ['2083','2082','2081','2080'],
